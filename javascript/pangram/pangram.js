21e4660dfd010813cc5cfab1e08d5d89d7527af3
Patrick McSweeny <patmcsweeny@gmail.com>
2020-01-02 10:01:17 -0500

    Improvement in JS pangram solution

--- javascript/pangram/pangram.js
+++ javascript/pangram/pangram.js
@@ -1,7 +1,8 @@
 const alphabet = "abcdefghijklmnopqrstuvwxyz";
 
 export const isPangram = (input) => {
+  input = input.toLowerCase();
   return alphabet.split("").every(function(letter) {
-    return input.toLowerCase().includes(letter);
+    return input.includes(letter);
   });
 };
